ad4ca1777091fb0894b8ccf754b52da95d2e6be6
Brett Wuebben <62609232+Brettw1419@users.noreply.github.com>
2022-07-20 16:53:26 -0400

    Finished Unit 8.2A

--- CharOffset/CharOffset.cpp
+++ CharOffset/CharOffset.cpp
@@ -10,13 +10,55 @@ char character(char start, int offset);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int i = 1;
+    cout << "Character offsets!\n\n";
+    try {
+        cout << "Trying test " << i << "!\n";
+        i++;
+        cout << character('a', 1) << endl;
+
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
+
+    try {
+        cout << "Trying test " << i << "!\n";
+        i++;
+        cout << character('a', -1) << endl;
+
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
+
+    try {
+        cout << "Trying test " << i << "!\n";
+        i++;
+        cout << character('Z', -1) << endl;
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
+
+    try {
+        cout << "Trying test " << i << "!\n";
+        i++;
+        cout << character('?', 5) << endl;
+    }
+    catch (exception e) {
+        cout << e.what() << endl;
+    }
 }
 
 char character(char start, int offset) {
     if (!isalpha(start)) {
-        throw invalidCharacterException();
+        throw invalid_argument("Must use a character for the starting character!");
     }
+    char target = start + offset;
+    if (!isalpha(target))
+        throw out_of_range("Offset must be within alphabet range!");
+    return target;
 }
 
 
